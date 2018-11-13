4b40b4730f25bb251e4544dabab4dc45ecd0a367
Syntthetix <garrett513r@gmail.com>
2018-11-13 15:22:46 -0500

    Added basic functionality
    - Added counter box/label
    - Added plus/minus buttons
    - Added reset button
    - Added target/method selection

--- Syns-Shiny-Counter-C-Edition/ShinyCounter.Designer.cs
+++ Syns-Shiny-Counter-C-Edition/ShinyCounter.Designer.cs
@@ -34,29 +34,41 @@
             this.whiteSmi = new System.Windows.Forms.ToolStripMenuItem();
             this.graySmi = new System.Windows.Forms.ToolStripMenuItem();
             this.blackSmi = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetSmi = new System.Windows.Forms.ToolStripMenuItem();
+            this.customSmi = new System.Windows.Forms.ToolStripMenuItem();
             this.helpMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.aboutSmi = new System.Windows.Forms.ToolStripMenuItem();
             this.githubSmi = new System.Windows.Forms.ToolStripMenuItem();
+            this.methodCombo = new System.Windows.Forms.ToolStripComboBox();
+            this.targetCombo = new System.Windows.Forms.ToolStripComboBox();
+            this.minusBtn = new System.Windows.Forms.Button();
+            this.plusBtn = new System.Windows.Forms.Button();
+            this.countBox = new System.Windows.Forms.NumericUpDown();
             this.menuStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.countBox)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip
             // 
             this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.editMenuItem,
-            this.helpMenuItem});
+            this.helpMenuItem,
+            this.methodCombo,
+            this.targetCombo});
             this.menuStrip.Location = new System.Drawing.Point(0, 0);
             this.menuStrip.Name = "menuStrip";
-            this.menuStrip.Size = new System.Drawing.Size(437, 24);
+            this.menuStrip.Size = new System.Drawing.Size(467, 27);
             this.menuStrip.TabIndex = 0;
             this.menuStrip.Text = "menuStrip1";
             // 
             // editMenuItem
             // 
             this.editMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.backgroundSmi});
+            this.backgroundSmi,
+            this.resetSmi,
+            this.customSmi});
             this.editMenuItem.Name = "editMenuItem";
-            this.editMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editMenuItem.Size = new System.Drawing.Size(39, 23);
             this.editMenuItem.Text = "Edit";
             // 
             // backgroundSmi
@@ -66,7 +78,7 @@
             this.graySmi,
             this.blackSmi});
             this.backgroundSmi.Name = "backgroundSmi";
-            this.backgroundSmi.Size = new System.Drawing.Size(138, 22);
+            this.backgroundSmi.Size = new System.Drawing.Size(147, 22);
             this.backgroundSmi.Text = "Background";
             // 
             // whiteSmi
@@ -90,34 +102,112 @@
             this.blackSmi.Text = "Black";
             this.blackSmi.Click += new System.EventHandler(this.blackSmi_Click);
             // 
+            // resetSmi
+            // 
+            this.resetSmi.Name = "resetSmi";
+            this.resetSmi.Size = new System.Drawing.Size(147, 22);
+            this.resetSmi.Text = "Reset to 0";
+            this.resetSmi.Click += new System.EventHandler(this.resetSmi_Click);
+            // 
+            // customSmi
+            // 
+            this.customSmi.Name = "customSmi";
+            this.customSmi.Size = new System.Drawing.Size(147, 22);
+            this.customSmi.Text = "Custom value";
+            this.customSmi.Click += new System.EventHandler(this.customSmi_Click);
+            // 
             // helpMenuItem
             // 
             this.helpMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.aboutSmi,
             this.githubSmi});
             this.helpMenuItem.Name = "helpMenuItem";
-            this.helpMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpMenuItem.Size = new System.Drawing.Size(44, 23);
             this.helpMenuItem.Text = "Help";
             // 
             // aboutSmi
             // 
             this.aboutSmi.Name = "aboutSmi";
-            this.aboutSmi.Size = new System.Drawing.Size(180, 22);
+            this.aboutSmi.Size = new System.Drawing.Size(173, 22);
             this.aboutSmi.Text = "About";
             this.aboutSmi.Click += new System.EventHandler(this.aboutSmi_Click);
             // 
             // githubSmi
             // 
             this.githubSmi.Name = "githubSmi";
-            this.githubSmi.Size = new System.Drawing.Size(180, 22);
+            this.githubSmi.Size = new System.Drawing.Size(173, 22);
             this.githubSmi.Text = "Open GitHub Page";
             this.githubSmi.Click += new System.EventHandler(this.githubSmi_Click);
+            // 
+            // methodCombo
+            // 
+            this.methodCombo.Items.AddRange(new object[] {
+            "RE\'s (Pre Gen 6)",
+            "RE\'s (Gen 6+)",
+            "Soft Resets (Pre Gen 6)",
+            "Soft Resets (Gen 6+)",
+            "Poke Radar (XY)",
+            "Poke Radar (DPP)",
+            "Chain Fishing"});
+            this.methodCombo.Name = "methodCombo";
+            this.methodCombo.Size = new System.Drawing.Size(145, 23);
+            this.methodCombo.Text = "Select method...";
+            this.methodCombo.ToolTipText = "Select your shiny hunting method here.";
+            // 
+            // targetCombo
+            // 
+            this.targetCombo.Name = "targetCombo";
+            this.targetCombo.Size = new System.Drawing.Size(145, 23);
+            this.targetCombo.Text = "Select target...";
+            // 
+            // minusBtn
+            // 
+            this.minusBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minusBtn.Location = new System.Drawing.Point(93, 334);
+            this.minusBtn.Name = "minusBtn";
+            this.minusBtn.Size = new System.Drawing.Size(79, 49);
+            this.minusBtn.TabIndex = 2;
+            this.minusBtn.Text = "-";
+            this.minusBtn.UseVisualStyleBackColor = true;
+            this.minusBtn.Click += new System.EventHandler(this.minusBtn_Click);
+            // 
+            // plusBtn
+            // 
+            this.plusBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plusBtn.Location = new System.Drawing.Point(284, 334);
+            this.plusBtn.Name = "plusBtn";
+            this.plusBtn.Size = new System.Drawing.Size(79, 49);
+            this.plusBtn.TabIndex = 3;
+            this.plusBtn.Text = "+";
+            this.plusBtn.UseVisualStyleBackColor = true;
+            this.plusBtn.Click += new System.EventHandler(this.plusBtn_Click);
+            // 
+            // countBox
+            // 
+            this.countBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.countBox.Font = new System.Drawing.Font("SAO UI", 65.24999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countBox.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.countBox.Location = new System.Drawing.Point(135, 107);
+            this.countBox.Maximum = new decimal(new int[] {
+            1316134911,
+            2328,
+            0,
+            0});
+            this.countBox.Name = "countBox";
+            this.countBox.ReadOnly = true;
+            this.countBox.Size = new System.Drawing.Size(182, 108);
+            this.countBox.TabIndex = 1;
+            this.countBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.countBox.ValueChanged += new System.EventHandler(this.countBox_ValueChanged);
             // 
             // ShinyCounter
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(437, 615);
+            this.ClientSize = new System.Drawing.Size(467, 646);
+            this.Controls.Add(this.countBox);
+            this.Controls.Add(this.plusBtn);
+            this.Controls.Add(this.minusBtn);
             this.Controls.Add(this.menuStrip);
             this.MainMenuStrip = this.menuStrip;
             this.Name = "ShinyCounter";
@@ -125,6 +215,7 @@
             this.Load += new System.EventHandler(this.ShinyCounter_Load);
             this.menuStrip.ResumeLayout(false);
             this.menuStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.countBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -141,6 +232,13 @@
         private System.Windows.Forms.ToolStripMenuItem helpMenuItem;
         private System.Windows.Forms.ToolStripMenuItem aboutSmi;
         private System.Windows.Forms.ToolStripMenuItem githubSmi;
+        private System.Windows.Forms.ToolStripMenuItem resetSmi;
+        private System.Windows.Forms.ToolStripMenuItem customSmi;
+        private System.Windows.Forms.ToolStripComboBox methodCombo;
+        private System.Windows.Forms.ToolStripComboBox targetCombo;
+        private System.Windows.Forms.Button minusBtn;
+        private System.Windows.Forms.Button plusBtn;
+        private System.Windows.Forms.NumericUpDown countBox;
     }
 }
 
--- Syns-Shiny-Counter-C-Edition/ShinyCounter.cs
+++ Syns-Shiny-Counter-C-Edition/ShinyCounter.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Syns_Shiny_Counter_C_Edition
 {
     public partial class ShinyCounter : Form
     {
+
+        int count = 0;
+
         public ShinyCounter()
         {
             InitializeComponent();
@@ -19,10 +23,60 @@ namespace Syns_Shiny_Counter_C_Edition
 
         private void ShinyCounter_Load(object sender, EventArgs e)
         {
-            this.BackColor = Color.DarkGray;
-            menuStrip.BackColor = Color.DarkGray;
+            fillPokemonList();
+            load();
+            countBox.Value = count;
         }
 
+        // Fill the targetCombo list with all pokemon
+        public void fillPokemonList()
+        {
+            List<string> pokemonList = new List<string>();
+            using (StreamReader sr = new StreamReader("pokemonlist.txt"))
+            {
+                string token;
+
+                while ((token = sr.ReadLine()) != null)
+                {
+                    pokemonList.Add(token);
+                }
+            }
+
+            foreach (string s in pokemonList)
+            {
+                targetCombo.Items.Add(s);
+            }
+        }
+
+        public void save()
+        {
+            using (StreamWriter sw = new StreamWriter("count.txt"))
+            {
+                sw.WriteLine(count.ToString());
+            }
+
+            using (StreamWriter sw = new StreamWriter("targetmethod.txt"))
+            {
+                sw.WriteLine(targetCombo.Text);
+                sw.WriteLine(methodCombo.Text);
+            }
+        }
+
+        public void load()
+        {
+            using (StreamReader sr = new StreamReader("count.txt"))
+            {
+                count = int.Parse(sr.ReadLine());
+            }
+
+            using (StreamReader sr = new StreamReader("targetmethod.txt"))
+            {
+                targetCombo.Text = sr.ReadLine();
+                methodCombo.Text = sr.ReadLine();
+            }
+        }
+
+        // Change background to white
         private void whiteSmi_Click(object sender, EventArgs e)
         {
             this.BackColor = Color.GhostWhite;
@@ -31,6 +85,7 @@ namespace Syns_Shiny_Counter_C_Edition
             helpMenuItem.ForeColor = Color.Black;
         }
 
+        // Change background to gray
         private void graySmi_Click(object sender, EventArgs e)
         {
             this.BackColor = Color.DarkGray;
@@ -39,6 +94,7 @@ namespace Syns_Shiny_Counter_C_Edition
             helpMenuItem.ForeColor = Color.Black;
         }
 
+        // Change background to black
         private void blackSmi_Click(object sender, EventArgs e)
         {
             this.BackColor = Color.Black;
@@ -47,15 +103,49 @@ namespace Syns_Shiny_Counter_C_Edition
             helpMenuItem.ForeColor = Color.White;
         }
 
+        // About page button
         private void aboutSmi_Click(object sender, EventArgs e)
         {
             AboutPage abp = new AboutPage();
             abp.ShowDialog();
         }
 
+        // Open GitHub Page button
         private void githubSmi_Click(object sender, EventArgs e)
         {
             System.Diagnostics.Process.Start("https://github.com/Syntthetix/Syns-Shiny-Counter-C-Edition");
+        }
+
+        private void customSmi_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void resetSmi_Click(object sender, EventArgs e)
+        {
+            count = 0;
+            countBox.Value = count;
+        }
+
+        // Increase counter by clicking on label
+        
+
+        private void minusBtn_Click(object sender, EventArgs e)
+        {
+            count--;
+            countBox.Value = count;
+        }
+
+        private void plusBtn_Click(object sender, EventArgs e)
+        {
+            count++;
+            countBox.Value = count;
+        }
+        
+        // Save data whenever count is updated
+        private void countBox_ValueChanged(object sender, EventArgs e)
+        {
+            save();
         }
     }
 }
